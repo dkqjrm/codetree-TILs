bb7671c04b0fe48bc2188ac8f7587a79e34f6a05
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-03-19 01:58:36 +0900

    Title: 특정 구간의 합, Time: 137ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240319/특정 구간의 합/sum-of-specific-intervals.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+A = list(map(int, input().split()))
+def solve(a1, a2):
+    result = 0
+    for i in range(a1-1, a2):
+        result += A[i]
+
+    return result
+
+for _ in range(m):
+    a1, a2 = map(int, input().split())
+    print(solve(a1, a2))
