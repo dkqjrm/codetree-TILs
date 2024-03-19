dda954016fbfb0cbe18b849f3fbe454590e928d4
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-03-19 15:45:50 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 130ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240319/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -0,0 +1,35 @@
+n = int(input())
+A = list(map(int, input().split()))
+
+# 2개씩 최소 공배수를 비교하고, 나오면 오른쪽꺼랑 비교하면 될 것 같음.
+def solve(result, n):
+    if n == 0:
+        tmp_a, tmp_b = result, 1
+        while tmp_b != 0:
+            r = tmp_a % tmp_b
+            tmp_a = tmp_b
+            tmp_b = r
+        result = (result * 1) // tmp_a
+        
+        return result
+
+    tmp_a, tmp_b = result, A[n]
+    while tmp_b != 0:
+        r = tmp_a % tmp_b
+        tmp_a = tmp_b
+        tmp_b = r
+    result = (result * A[n]) // tmp_a
+
+    return solve(result, n-1)
+
+
+print(solve(1, n-1))
+    
+    
+
+
+# lcm(lcm(A[2], A[1]), A[0])
+# lcm(n, n-1) -> n과 n-1의 최소공배수
+# lcm(lcm(n, n-1), n-2) n과 n-1와 n-2의 최소공배수
+
+# -> n-1과 n-2의 최소 공배수를 return 하고, 그 값과 n-3의 값을 return 하면 됨.
