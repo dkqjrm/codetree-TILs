741dd53b43a23fcee4fc95a800884db17dba405b
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-03-20 18:19:48 +0900

    Title: 줄 세우기 2, Time: 150ms, Memory: 25MB, Status: Passed - CodeTree

--- 240320/줄 세우기 2/line-up-students-2.py
+++ 240320/줄 세우기 2/line-up-students-2.py
@@ -1,10 +1,20 @@
+class Student:
+    def __init__(self, height, weight, idx):
+        self.height = height
+        self.weight = weight
+        self.idx = idx
+
+    def __str__(self):
+        return f'{self.height} {self.weight} {self.idx}'
+
 n = int(input())
-students = []
+
+student_list = []
 for idx in range(1, n+1):
-    h, w = map(int,input().split())
-    students.append((h, w, idx))
+    h, w = map(int, input().split())
+    student_list.append(Student(h, w, idx))
 
-students.sort()
+student_list.sort(key = lambda x : (x.height, -x.weight))
 
-for student in students:
-    print(*student)
+for student in student_list:
+    print(student)
