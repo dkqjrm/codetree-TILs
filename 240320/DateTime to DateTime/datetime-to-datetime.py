fffcadf848a39628b9f395b87a85daec50563e3d
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-03-20 22:58:48 +0900

    Title: DateTime to DateTime, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240320/DateTime to DateTime/datetime-to-datetime.py
+++ 240320/DateTime to DateTime/datetime-to-datetime.py
@@ -3,9 +3,12 @@ a, b, c = map(int, input().split())
 # 11 11 - 671
 # 24시간 1440
 
-if a == 11:
-    print(b * 60 + c - 671)
-elif a == 12:
-    print(769 + b * 60 + c)
+if 11 * 1440 + 11 * 60 + 11 < a * 1440 + b * 60 + c:
+    if a == 11:
+        print(b * 60 + c - 671)
+    elif a == 12:
+        print(769 + b * 60 + c)
+    else:
+        print(769 + 1440 + b * 60 + c)
 else:
-    print(769 + 1440 + b * 60 + c)
+    print(-1)
