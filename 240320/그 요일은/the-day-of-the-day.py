0dfbff4f5061d82eb0ae1f0fb310c368f3e1d66d
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-03-20 23:26:15 +0900

    Title: 그 요일은, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240320/그 요일은/the-day-of-the-day.py
+++ 240320/그 요일은/the-day-of-the-day.py
@@ -3,13 +3,13 @@ A = input()
 
 num_of_days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 day_of_the_week = {
-    'Mon' : 1,
-    'Tue' : 2,
-    'Wed' : 3,
-    'Thu' : 4,
-    'Fri' : 5,
-    'Sat' : 6,
-    'Sun' : 7}
+    'Mon' : 0,
+    'Tue' : 1,
+    'Wed' : 2,
+    'Thu' : 3,
+    'Fri' : 4,
+    'Sat' : 5,
+    'Sun' : 6}
 
 first_days = 0
 for i in range(m1 - 1):
@@ -21,7 +21,7 @@ for i in range(m2 - 1):
     second_days += num_of_days[i]
 second_days += d2
 
-print((second_days - first_days) // 7 + 1 + int((second_days - first_days) % 7 >= day_of_the_week[A]))
+print((second_days - first_days) // 7 + int((second_days - first_days) % 7 >= day_of_the_week[A]))
 
 # day_of_the_week = {
 #     'Mon' : 0,
