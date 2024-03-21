9b87988afb75cb5c4ba477b6a3bd41a07c978628
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-03-21 18:26:26 +0900

    Title: 연속되는 수 3, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240321/연속되는 수 3/continuous-number3.py
@@ -0,0 +1,14 @@
+n = int(input())
+A_list = []
+for _ in range(n):
+    A_list.append(input())
+
+ans = 0
+for idx in range(len(A_list)):
+    if idx == 0 or (A_list[idx-1][0] != A_list[idx][0]):
+        cnt = 1
+    else:
+        cnt += 1
+    ans = max(ans, cnt)
+
+print(ans)
