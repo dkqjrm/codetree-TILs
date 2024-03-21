6f578b27b910553f9f6ef7d51b6046a50f27ef2d
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-03-21 09:28:26 +0900

    Title: 10진수로 변환하기, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240321/10진수로 변환하기/convert-to-decimal.py
+++ 240321/10진수로 변환하기/convert-to-decimal.py
@@ -1,15 +1,15 @@
 n = int(input())
 
-result = 0
-for i in str(n):
-    result = result * 2 + int(i)
-
-print(result)
-
 # result = 0
-# mul = 1
-# while n != 0:
-#     result += (n % 10) * mul
-#     n //= 10
-#     mul *= 2
+# for i in str(n):
+#     result = result * 2 + int(i)
+
 # print(result)
+
+result = 0
+mul = 1
+while n != 0:
+    result += (n % 10) * mul
+    n //= 10
+    mul *= 2
+print(result)
