c6dda1a66a0ff05f52ad4c6d1b3c2c58940e1e2c
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-03-21 10:27:59 +0900

    Title: 왔다 갔던 구역 2, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240321/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240321/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -6,12 +6,12 @@ visited = [0] * 410
 def move(location, x, arrow):
     if arrow == 'L':
         after_location = location - int(x)
-        for i in range(location, after_location, -1):
+        for i in range(location - 1, after_location - 1, -1): # 6L 이면 201 201 200 109 108 107을 체크함.
             visited[i] += 1
 
     else:
         after_location = location + int(x)
-        for i in range(location, after_location):
+        for i in range(location, after_location): # 2R 이면 200, 201을 체크함
             visited[i] += 1
 
     return after_location
