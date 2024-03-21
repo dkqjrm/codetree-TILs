ba1f7f32ae4d03f0a0cedaed778161a3cb34f375
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-03-21 19:27:13 +0900

    Title: 선두를 지켜라, Time: 130ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240321/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,28 @@
+n, m = map(int, input().split())
+
+a_list = [0]
+b_list = [0]
+for _ in range(n):
+    v, t = map(int, input().split())
+    for time in range(t):
+        a_list.append(a_list[-1] + v)
+
+for _ in range(m):
+    v, t = map(int, input().split())
+    for time in range(t):
+        b_list.append(b_list[-1] + v)
+
+winner = None
+cnt = 0
+for idx in range(len(a_list)):
+    if a_list[idx] > b_list[idx]:
+        if winner == 'B':
+            cnt += 1
+        winner = 'A'
+    else:
+        if winner == 'A':
+            cnt += 1
+        winner = 'B'
+# print(a_list)
+# print(b_list)
+print(cnt)
