157e071e3addfdc6e43f3149a91415826b304152
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-03-21 13:14:28 +0900

    Title: 흰검 칠하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240321/흰검 칠하기/painting-white-black.py
+++ 240321/흰검 칠하기/painting-white-black.py
@@ -1,46 +1,66 @@
 OFFSET = 200
 
 n = int(input())
+# n = 5
 
-visited = [('Blank', 0, 0)] * 400
+visited = [('BLANK', 0, 0)] * 400
+
+# def move(location, x, arrow):
+#     x = int(x)
+#     if x == 1:
+#     if arrow == 'L':
+#         new_location = location - x
+#         for i in range(new_location, location):
+#             visited[i] = (0, visited[i][1] + 1, visited[i][2])
+#             if visited[i][1] >= 2 and visited[i][2] >= 2:
+#                 visited[i] = (2, visited[i][1], visited[i][2])
+            
+#     else:
+#         if x == 1:
+#             x = 0
+#         new_location = location + x
+#         for i in range(location, new_location):
+#             visited[i] = (1, visited[i][1], visited[i][2] + 1)
+#             if visited[i][1] >= 2 and visited[i][2] >= 2:
+#                 visited[i] = (2, visited[i][1], visited[i][2])
+#     return new_location
 
 def move(location, x, arrow):
     x = int(x)
-    if x == 1:
-        x += 1
     if arrow == 'L':
-        new_location = location - x
-        for i in range(new_location, location):
-            visited[i] = (0, visited[i][1] + 1, visited[i][2])
-            if visited[i][1] >= 2 and visited[i][2] >= 2:
-                visited[i] = (2, visited[i][1], visited[i][2])
-            
+        visited[location] = (0, visited[location][1] + 1, visited[location][2])
+        if visited[location][1] >= 2 and visited[location][2] >= 2:
+            visited[location] = (2, visited[location][1], visited[location][2])
+        for _ in range(1,x):
+            location -= 1
+            visited[location] = (0, visited[location][1] + 1, visited[location][2])
+            if visited[location][1] >= 2 and visited[location][2] >= 2:
+                visited[location] = (2, visited[location][1], visited[location][2])
     else:
-        new_location = location + x
-        for i in range(location, new_location):
-            visited[i] = (1, visited[i][1], visited[i][2] + 1)
-            if visited[i][1] >= 2 and visited[i][2] >= 2:
-                visited[i] = (2, visited[i][1], visited[i][2])
-            
-    return new_location
+        visited[location] = (1, visited[location][1], visited[location][2] + 1)
+        if visited[location][1] >= 2 and visited[location][2] >= 2:
+            visited[location] = (2, visited[location][1], visited[location][2])
+        for _ in range(1,x):
+            location += 1
+            visited[location] = (1, visited[location][1], visited[location][2] + 1)
+            if visited[location][1] >= 2 and visited[location][2] >= 2:
+                visited[location] = (2, visited[location][1], visited[location][2])
+    return location
 
 location = OFFSET
 for _ in range(n):
     x, arrow = input().split()
     location = move(location, x, arrow)
+# for i in [[1,'L'],[1,'L'],[1,'R'],[2,'R'],[1,'L']]:
+#     x, arrow = i
+#     location = move(location, x, arrow)
+    # print(visited[198:202])
 
-white = 0
-black = 0
-grey = 0
-for i in visited:
-    if i[0] == 0:
-        white += 1
-    elif i[0] == 1:
-        black += 1
-    elif i[0] == 2:
-        grey += 1
+white = sum(1 for i in visited if i[0] == 0)
+black = sum(1 for i in visited if i[0] == 1)
+grey = sum(1 for i in visited if i[0] == 2)
 
-# print(visited)
+
 print(f'{white} {black} {grey}')
 
 #     200 201 202 203
