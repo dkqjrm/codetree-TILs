56761c23550b79f8771c56f65f551fa24dddc019
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-03-22 19:45:43 +0900

    Title: 작은 구슬의 이동, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240322/작은 구슬의 이동/small-marble-movement.py
@@ -0,0 +1,42 @@
+n, t = map(int, input().split())
+r, c, d = input().split()
+r, c = int(r) - 1, int(c) - 1 # 0 1
+
+matrix = [[0] * n for _ in range(n)]
+
+dc = {
+    'U': 0,
+    'D': 0,
+    'R': 1,
+    'L': -1
+}
+
+dr = {
+    'U': 1,
+    'D': -1,
+    'R': 0,
+    'L': 0,
+}
+
+def in_range(r, c, n):
+    return 0 <= r and r < n and 0 <= c and c < n
+
+for _ in range(t):
+    move_r = r + dr[d]
+    move_c = c + dc[d] 
+    # 만약 이동하려는 곳이 range 안이라면 앞으로 이동
+    if in_range(move_r, move_c, n):
+        r = move_r
+        c = move_c
+    # 밖이라면 방향 전환
+    else:
+        if d == 'U':
+            d = 'D'
+        elif d == 'R':
+            d = 'L'
+        elif d == 'D':
+            d = 'U'
+        elif d == 'L':
+            d = 'R'
+
+print(r + 1, c + 1) # 0 2
