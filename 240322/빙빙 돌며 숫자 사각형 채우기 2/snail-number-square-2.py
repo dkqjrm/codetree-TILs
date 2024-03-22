94d7900a5e1553246ac41351cda1b428befde513
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-03-22 23:09:05 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 127ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240322/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
@@ -0,0 +1,30 @@
+row, column = map(int, input().split())
+
+matrix = [[0] * column for _ in range(row)]
+
+def in_range(r, c, row, column):
+    return 0 <= r and r < row and 0 <= c and c < column
+
+dr = [1, 0, -1, 0] # 남 동 북 서
+dc = [0, 1, 0, -1] # 남 동 북 서
+
+direction = 0
+
+r, c = 0, 0
+idx = 1
+
+while idx != (row * column):
+    matrix[r][c] = idx
+    move_r = r + dr[direction]
+    move_c = c + dc[direction]
+    if in_range(move_r, move_c, row, column) and matrix[move_r][move_c] == 0:
+        r = move_r
+        c = move_c
+        idx += 1
+        matrix[r][c] = idx
+    else:
+        direction = (direction + 1) % 4
+
+
+for rows in matrix:
+    print(*rows)
