7255b80786f3a74e3ee1cbf0a4c51bc4f811c977
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-03-24 01:15:29 +0900

    Title: Carry 피하기 2, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240324/Carry 피하기 2/escaping-carry-2.py
+++ 240324/Carry 피하기 2/escaping-carry-2.py
@@ -1,9 +1,10 @@
 n = int(input())
 array = [int(input()) for _ in range(n)]
 
+
 def solve(num1, num2, num3):
-    while num1 != 0 and num2 != 0 and num3 != 0:
-        if num1 % 10 + num2 % 10 + num3 % 10 > 10:
+    while num1 != 0 or num2 != 0 or num3 != 0:
+        if num1 % 10 + num2 % 10 + num3 % 10 >= 10:
             return False
         else:
             num1 //= 10
@@ -18,5 +19,6 @@ for i in range(n):
             # i, j, k는 각각 숫자임
             if solve(array[i], array[j], array[k]):
                 result = max(result, array[i]+array[j]+array[k])
+                # print(array[i], array[j], array[k])
 
 print(result)
