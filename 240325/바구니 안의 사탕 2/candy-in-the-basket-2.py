219e7de76bf3e3dc943f0b909d5fca8969186e03
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-03-25 01:49:29 +0900

    Title: 바구니 안의 사탕 2, Time: 114ms, Memory: 25MB, Status: Passed - CodeTree

--- 240325/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240325/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,12 +1,12 @@
 n, k = map(int, input().split())
 
-array = [0] * 500
+array = [0] * 105
 for _ in range(n):
     num, location = map(int, input().split())
     array[location] += num
 
 result = 0
-for i in range(k, len(array)):
+for i in range(len(array)):
     result = max(result, sum(array[i - k: i + k + 1]))
         # print(array[i - k: i + k + 1], i - k, i + k + 1)
 
