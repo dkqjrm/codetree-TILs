e81d17fc8ca9ef8bef4b65bd988f3c06d0feabc6
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-03-25 01:37:38 +0900

    Title: 바구니 안의 사탕 2, Time: 120ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240325/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240325/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -6,7 +6,7 @@ for _ in range(n):
     array[location] = num
 
 result = 0
-for i in range(k, len(array) - k):
+for i in range(k, len(array) - k + 2):
     result = max(result, sum(array[i - k: i + k + 1]))
     # print(array[i - k : i + k + 1])
 
