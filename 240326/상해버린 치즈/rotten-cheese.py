9b02f1528cf5e90e53c34f6d6d772ffb6074d635
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-03-26 00:30:56 +0900

    Title: 상해버린 치즈, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240326/상해버린 치즈/rotten-cheese.py
+++ 240326/상해버린 치즈/rotten-cheese.py
@@ -5,20 +5,22 @@ pmt_array = [list(map(int, input().split())) for _ in range(d)]
 pt_array = [list(map(int, input().split())) for _ in range(s)]
 
 
-# 1, 2, 4
-# 사람이 t초 전에 먹은 치즈는 다 의심 대상임.
-inspect_list = []
+# 치즈는 하나만 상했음 pt에서 각각의 p가 t전에 먹은 치즈의 교집합
+final_inspect_list = []
 for pt in pt_array:
-    # pt[0]이 pt[1] 전에 먹은 치즈를 찾자.
+    inspect_list = []
     for pmt in pmt_array:
-        if pmt[0] == pt[0] and pmt[2] < pt[1]:
+        if pt[0] == pmt[0] and pt[1] > pmt[2]:
             inspect_list.append(pmt[1])
+    final_inspect_list.append(set(inspect_list))
 
-inspect_list = set(inspect_list)
-
+final_inspect = final_inspect_list[0]
+for idx in range(1, len(final_inspect_list)):
+    final_inspect = final_inspect & final_inspect_list[idx]
+    
 suspect_list = []
 for pmt in pmt_array:
-    if pmt[1] in inspect_list:
+    if pmt[1] in final_inspect:
         suspect_list.append(pmt[0])
 
 print(len(set(suspect_list)))
