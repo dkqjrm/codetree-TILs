735de2c478e2fb44c0e1b9308cc74ef11ced2d21
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-03-25 17:58:53 +0900

    Title: 개발자의 능력 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240325/개발자의 능력 2/ability-of-developer-2.py
@@ -0,0 +1,18 @@
+array = list(map(int, input().split()))
+
+total = sum(array)
+
+result = 1e9
+
+for i in range(len(array)):
+    for j in range(i+1, len(array)):
+        for k in range(len(array)):
+            if k != i:
+                for l in range(k+1, len(array)):
+                    if j != l:
+                        team1 = array[i] + array[j]
+                        team2 = array[k] + array[l]
+                        team3 = total - team1 - team2
+                        result = min(result, max(team1, team2, team3) - min(team1, team2, team3))
+
+print(result)
