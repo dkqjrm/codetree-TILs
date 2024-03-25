fab2dbee649020aacbc3c7249851c6e60e761898
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-03-25 17:59:49 +0900

    Title: 개발자의 능력 2, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240325/개발자의 능력 2/ability-of-developer-2.py
+++ 240325/개발자의 능력 2/ability-of-developer-2.py
@@ -7,9 +7,9 @@ result = 1e9
 for i in range(len(array)):
     for j in range(i+1, len(array)):
         for k in range(len(array)):
-            if k != i:
+            if k != i and k != j:
                 for l in range(k+1, len(array)):
-                    if j != l:
+                    if l != j and l != i:
                         team1 = array[i] + array[j]
                         team2 = array[k] + array[l]
                         team3 = total - team1 - team2
