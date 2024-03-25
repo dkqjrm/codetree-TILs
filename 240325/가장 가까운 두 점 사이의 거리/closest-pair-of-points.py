ab18175d0507b8422d67acf2340b60d04831bdbb
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-03-25 22:27:02 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 130ms, Memory: 25MB, Status: Passed - CodeTree

--- 240325/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
+++ 240325/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
@@ -5,6 +5,6 @@ min_length = 1e13
 for i in array:
     for j in array:
         if i != j:
-            min_length = min(min_length, ((i[0]-j[1]) ** 2) + ((i[1]-j[1]) ** 2))
+            min_length = min(min_length, ((i[0]-j[0]) ** 2) + ((i[1]-j[1]) ** 2))
 
 print(min_length)
