bff73c5e922feb45c8068a3e335a4bd84acd5ee5
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-03-26 21:53:08 +0900

    Title: 정보에 따른 숫자 2, Time: 129ms, Memory: 25MB, Status: Passed - CodeTree

--- 240326/정보에 따른 숫자 2/number-based-on-information-2.py
+++ 240326/정보에 따른 숫자 2/number-based-on-information-2.py
@@ -10,7 +10,7 @@ cnt = 0
 for k in range(a, b + 1):
     length_S = 1e9
     length_N = 1e9
-    for i in range(a, b + 1):
+    for i in range(len(array)):
         if array[i] == 'S':
             length_S = min(length_S, abs(k - i))
         elif array[i] == 'N':
@@ -18,6 +18,5 @@ for k in range(a, b + 1):
 
     if length_S <= length_N:
         cnt += 1
-
     
 print(cnt)
