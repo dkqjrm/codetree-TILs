f7d4eecab8246feed06ce661be463a25b741f95b
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-03-26 15:55:45 +0900

    Title: 스승의 은혜 3, Time: 400ms, Memory: 25MB, Status: Passed - CodeTree

--- 240326/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240326/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -1,39 +1,39 @@
-# n, b = map(int, input().split())
+n, b = map(int, input().split())
 
-# array = [list(map(int, input().split())) for _ in range(n)]
+array = [list(map(int, input().split())) for _ in range(n)]
 
 
-# max_cnt = 0
-# for i in range(n):
-#     array[i][0] //= 2
-#     cnt = 0
-#     price = 0
-#     for student in sorted(array, key = lambda x : x[0] + x[1]):
-#         if price + student[0] + student[1] <= b:
-#             cnt += 1
-#             price += student[0] + student[1]
-#     max_cnt = max(max_cnt, cnt)
-#     array[i][0] *= 2
-
-# print(max_cnt)
-    
-n, b = map(int, input('').split(' '))
-pre = []
+max_cnt = 0
 for i in range(n):
-    q, w = map(int, input('').split(' '))
-    pre.append([q+w, q, w])
-pre.sort()
-
-cnt = 0
-s = 0
-
-for i in range(n):
-    price = pre[i][0]
-    if s + price <= b: # 예산 미초과
-       s += price
-       cnt += 1
-    else : # 예산 초과
-        if s + (pre[i][1]/ 2)+ pre[i][2] <= b: # 쿠폰 쓰면 safe
+    array[i][0] //= 2
+    cnt = 0
+    price = 0
+    for student in sorted(array, key = lambda x : x[0] + x[1]):
+        if price + student[0] + student[1] <= b:
             cnt += 1
-            break
-print(cnt)
+            price += student[0] + student[1]
+    max_cnt = max(max_cnt, cnt)
+    array[i][0] *= 2
+
+print(max_cnt)
+    
+# n, b = map(int, input('').split(' '))
+# pre = []
+# for i in range(n):
+#     q, w = map(int, input('').split(' '))
+#     pre.append([q+w, q, w])
+# pre.sort()
+
+# cnt = 0
+# s = 0
+
+# for i in range(n):
+#     price = pre[i][0]
+#     if s + price <= b: # 예산 미초과
+#        s += price
+#        cnt += 1
+#     else : # 예산 초과
+#         if s + (pre[i][1]/ 2)+ pre[i][2] <= b: # 쿠폰 쓰면 safe
+#             cnt += 1
+#             break
+# print(cnt)
