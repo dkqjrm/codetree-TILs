ff9ad48aba102d1b56668b72496a8a499e3e9163
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-01 13:45:53 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240401/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.py
+++ 240401/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.py
@@ -1,12 +1,31 @@
+# a, b, c = map(int, input().split())
+
+# max_num = 0
+# for a_cnt in range(0, (1000//a) + 1):
+#     for b_cnt in range(0, (1000//b) + 1):
+#         tmp = a * a_cnt + b * b_cnt
+#         if tmp <= c:
+#             max_num = max(tmp, max_num)
+#         else:
+#             break
+
+
+# 변수 선언 및 입력
 a, b, c = map(int, input().split())
 
 max_num = 0
-for a_cnt in range(0, (1000//a) + 1):
-    for b_cnt in range(0, (1000//b) + 1):
-        tmp = a * a_cnt + b * b_cnt
-        if tmp <= c:
-            max_num = max(tmp, max_num)
-        else:
-            break
 
-print(max_num)
+# a를 몇 회 사용할지 전부 시도해 봅니다.
+# 모든 경우의 수에 대해 최대가 되도록 하는 수를 계산합니다.
+for i in range(c // a + 1):
+    # a를 i회 사용합니다.
+    cnt = a * i
+
+    # 값을 최대로 하기 위해 b를 몇회 사용해야 하는지 계산합니다.
+    num_b = (c - cnt) // b
+
+    cnt += num_b * b
+    
+    ans = max(ans, cnt)
+
+print(ans)
