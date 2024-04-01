1a858b2fdc1f8e28e1c808bd895d6c14aec9ff04
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-01 12:31:02 +0900

    Title: 팰린드롬 수 찾기, Time: 192ms, Memory: 25MB, Status: Passed - CodeTree

--- 240401/팰린드롬 수 찾기/find-the-number-of-palindrome.py
+++ 240401/팰린드롬 수 찾기/find-the-number-of-palindrome.py
@@ -2,11 +2,12 @@ x, y = map(int, input().split())
 
 def checking(num):
     num = str(num)
-    for i in range(len(num)//2):
-        if num[i] != num[len(num) - 1 - i]:
-            return False
-
-    return True
+    # for i in range(len(num)//2):
+    #     if num[i] != num[len(num) - 1 - i]:
+    #         return False
+    if num == num[::-1]:
+        return True
+    return False
 
 cnt = 0
 for num in range(x, y + 1):
