4a1a57a65226ee9979c0cdeb0c04470bfc90858d
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-01 13:44:14 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240401/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.py
+++ 240401/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.py
@@ -6,7 +6,5 @@ for a_cnt in range(1, (1000//a) + 2):
         tmp = a * a_cnt + b * b_cnt
         if tmp <= c:
             max_num = max(tmp, max_num)
-        else:
-            break
 
 print(max_num)
