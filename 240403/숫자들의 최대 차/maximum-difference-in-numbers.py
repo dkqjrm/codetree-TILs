781ecf9f1958cd0d376385af71edb3beb6bf027e
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-03 00:08:47 +0900

    Title: 숫자들의 최대 차, Time: 124ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240403/숫자들의 최대 차/maximum-difference-in-numbers.py
@@ -0,0 +1,12 @@
+n, k = map(int, input().split())
+array = [int(input()) for _ in range(n)]
+max_cnt = 0
+
+for i in range(len(array)): # 인덱스 i는 최솟값
+    cnt = 0
+    for j in range(len(array)):
+        if 0 <= array[j] - array[i] and array[j] - array[i] <= 3:
+            cnt += 1
+    max_cnt = max(cnt, max_cnt)
+
+print(max_cnt)
