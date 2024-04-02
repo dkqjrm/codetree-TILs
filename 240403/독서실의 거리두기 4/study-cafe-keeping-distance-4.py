375e711429bfb8bfd74729b6c2dd96bf4d26a4f9
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-03 01:38:40 +0900

    Title: 독서실의 거리두기 4, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240403/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
@@ -0,0 +1,32 @@
+n = int(input())
+array = list(map(int, list(input())))
+
+def check(array):
+    check = False
+    min_cnt = 1e12
+    for i in array:
+        if check is False and i == 1:
+            check = True
+            cnt = 0
+        elif check is True and i == 0:
+            cnt += 1
+        elif check is True and i == 1:
+            cnt += 1
+            min_cnt = min(cnt, min_cnt)
+            cnt = 0
+    return min_cnt
+
+
+max_cnt = 0
+
+for i in range(n):
+    for j in range(i, n):
+        if array[i] == 0 and array[j] == 0:
+            array[i] = 1
+            array[j] = 1
+            cnt = check(array)
+            max_cnt = max(cnt, max_cnt)
+            array[i] = 0
+            array[j] = 0
+        
+print(max_cnt)
