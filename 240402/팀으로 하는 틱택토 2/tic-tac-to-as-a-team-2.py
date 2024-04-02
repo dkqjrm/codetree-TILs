f987f097ef850f002cbe98bf7dff83f14651fa2f
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-02 15:27:11 +0900

    Title: 팀으로 하는 틱택토 2, Time: 209ms, Memory: 24MB, Status: Passed - CodeTree

--- 240402/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 240402/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -7,16 +7,16 @@ for i in array:
 
 # print(array)
 
-if len(set([array[0][0], array[1][0], array[2][0]])) == 2:
+if len(set([array[0][0], array[1][0], array[2][0]])) == 2 and set([array[0][0], array[1][0], array[2][0]]) not in result:
     result.append(set([array[0][0], array[1][0], array[2][0]]))
-if len(set([array[0][1], array[1][1], array[2][1]])) == 2:
+if len(set([array[0][1], array[1][1], array[2][1]])) == 2 and set([array[0][1], array[1][1], array[2][1]]) not in result:
     result.append(set([array[0][1], array[1][1], array[2][1]]))
-if len(set([array[0][2], array[1][2], array[2][2]])) == 2:
+if len(set([array[0][2], array[1][2], array[2][2]])) == 2 and set([array[0][2], array[1][2], array[2][2]]) not in result:
     result.append(set([array[0][2], array[1][2], array[2][2]]))
 
-if len(set([array[0][0], array[1][1], array[2][2]])) == 2:
+if len(set([array[0][0], array[1][1], array[2][2]])) == 2 and set([array[0][0], array[1][1], array[2][2]]) not in result:
     result.append(set([array[0][0], array[1][1], array[2][2]]))
-if len(set([array[0][2], array[1][1], array[2][0]])) == 2:
+if len(set([array[0][2], array[1][1], array[2][0]])) == 2 and set([array[0][2], array[1][1], array[2][0]]) not in result:
     result.append(set([array[0][2], array[1][1], array[2][0]]))
 
 print(len(result))
