24dfccf8d1e0440e40fdbda3dd3eb3cbce7bbeab
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-02 13:52:36 +0900

    Title: 좌표평면 위의 균형 2, Time: 154ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240402/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
@@ -0,0 +1,26 @@
+n = int(input())
+array = [list(map(int, input().split())) for _ in range(n)]
+
+# 하지만 사사분면 ++ -+ -- +-
+
+min_m = 1e12
+
+for x in range(1, 101):
+    for y in range(1, 101):
+        pp = 0
+        mp = 0
+        mm = 0
+        pm = 0
+        for i in array:
+            if i[0] < x and i[1] < y:
+                mm += 1
+            elif i[0] < x and i[1] > y:
+                mp += 1
+            elif i[0] > x and i[1] < y:
+                pm += 1
+            else:
+                pp += 1
+        M = max(pp, mp, mm, pm)
+        min_m = min(min_m, M)
+
+print(min_m)
