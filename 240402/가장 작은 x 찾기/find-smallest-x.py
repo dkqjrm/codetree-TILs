64f66c7147f93f33d565af678e68860dc8b0fbf3
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-02 23:39:39 +0900

    Title: 가장 작은 x 찾기, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240402/가장 작은 x 찾기/find-smallest-x.py
@@ -0,0 +1,20 @@
+n = int(input())
+array = [list(map(int, input().split())) for _ in range(n)]
+
+
+def check(x):
+    for i in array:
+        x *= 2
+        if x < i[0] or x > i[1]:
+            return 0
+        
+    return 1
+
+min_x = 1e12
+
+for x in range(1, array[0][1] // 2):
+    if check(x):
+        min_x = min(x, min_x)
+    
+
+print(min_x)
