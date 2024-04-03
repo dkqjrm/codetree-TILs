687097eaba73dbaafa2c254791a24d1d391217d0
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-03 19:36:28 +0900

    Title: 구간 잘 나누기, Time: 180ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240403/구간 잘 나누기/divide-sections-well.py
+++ 240403/구간 잘 나누기/divide-sections-well.py
@@ -3,15 +3,13 @@ array = list(map(int, input().split()))
 min_result = 1e12
 
 for maxi in range(1, 10001): # max
-    tmp_result = [0]
-    for i in array:
-        if tmp_result[-1] + i > maxi:
-            tmp_result.append(i)
+    tmp_result = [array[0]]
+    for idx in range(1, len(array)):
+        if tmp_result[-1] + array[idx] > maxi:
+            tmp_result.append(array[idx])
         else:
-            tmp_result[-1] += i
+            tmp_result[-1] += array[idx]
 
     if len(tmp_result) == m:
         min_result = min(min_result, max(tmp_result))
-
-    # print(tmp_result)
 print(min_result)
