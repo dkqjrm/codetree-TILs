112aa25d4e47b76fe49c54d76d389f217554d144
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-03 16:42:44 +0900

    Title: 두 선분, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240403/두 선분/two-lines.py
+++ 240403/두 선분/two-lines.py
@@ -5,7 +5,7 @@ def solve(x1, x2, x3, x4):
     for i in range(x1, x2 + 1):
         visited[i] = 1
     for j in range(x3, x4 + 1):
-        if visited[i] == 1:
+        if visited[j] == 1:
             return "intersecting"
     
     return "nonintersecting"
