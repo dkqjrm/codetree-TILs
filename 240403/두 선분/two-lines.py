059915bd41f295737ac74da7e602aba55fb9aaee
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-03 16:43:30 +0900

    Title: 두 선분, Time: 176ms, Memory: 24MB, Status: Passed - CodeTree

--- 240403/두 선분/two-lines.py
+++ 240403/두 선분/two-lines.py
@@ -1,13 +1,9 @@
 x1, x2, x3, x4 = map(int, input().split())
 
 def solve(x1, x2, x3, x4):
-    visited = [0] * 101
-    for i in range(x1, x2 + 1):
-        visited[i] = 1
-    for j in range(x3, x4 + 1):
-        if visited[j] == 1:
-            return "intersecting"
-    
-    return "nonintersecting"
+    if x2 < x3 or x4 < x1:
+        return "nonintersecting"
+    else:
+        return "intersecting"
 
 print(solve(x1, x2, x3, x4))
