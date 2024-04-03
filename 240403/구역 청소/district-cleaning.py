b789afbf49ecce655f425a3a18c37d765bd0a71a
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-03 17:30:46 +0900

    Title: 구역 청소, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240403/구역 청소/district-cleaning.py
+++ 240403/구역 청소/district-cleaning.py
@@ -3,14 +3,14 @@ c, d = map(int, input().split())
 
 if b < c or d < a:
     result = b - a + d - c
-elif c < b and b < d:
+elif a < c and c < b and b < d:
     result = c - a + b - c + d - b
-elif a < d and d < b:
+elif c < a and a < d and d < b:
     result = a - c + d - a + b - d
 
-elif a < c and d < b:
+elif a <= c and d <= b:
     result = b - a
-elif c < a and b < d:
+elif c <= a and b <= d:
     result = d - c
 
 print(result)
