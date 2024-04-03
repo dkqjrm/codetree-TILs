4185ae204085df907ad014c18b28ca2addde6bfa
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-04 01:01:19 +0900

    Title: 전부 겹치는 선분, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240404/전부 겹치는 선분/overlapping-line-segments.py
+++ 240404/전부 겹치는 선분/overlapping-line-segments.py
@@ -2,16 +2,27 @@ n = int(input())
 array = [list(map(int, input().split())) for _ in range(n)]
 
 
-check = False
-for x in range(1, 101):
-    cnt = 0
-    for i in array:
-        if i[0] <= x and x <= i[1]:
-            cnt += 1
-    if cnt == n:
-        check = True
-        print("Yes")
-        break
+# check = False
+# for x in range(1, 101):
+#     cnt = 0
+#     for i in array:
+#         if i[0] <= x and x <= i[1]:
+#             cnt += 1
+#     if cnt == n:
+#         check = True
+#         print("Yes")
+#         break
 
-if check is False:
+# if check is False:
+#     print("No")
+    
+max_x1 = 0
+min_x2 = 1e12
+for i in array:
+    max_x1 = max(max_x1, i[0])
+    min_x2 = min(min_x2, i[1])
+
+if min_x2 >= max_x1:
+    print("Yes")
+else:
     print("No")
