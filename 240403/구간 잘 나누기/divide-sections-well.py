cfb06fc2e0e0c5d76f6f644b2940e8232f4bb051
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-03 19:15:17 +0900

    Title: 구간 잘 나누기, Time: 127ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240403/구간 잘 나누기/divide-sections-well.py
+++ 240403/구간 잘 나누기/divide-sections-well.py
@@ -2,17 +2,32 @@ n, m = map(int, input().split())
 array = list(map(int, input().split()))
 min_result = 1e12
 
-for i in range(len(array)):
-    tmp_array = []
-    maxi = sum(array[:i + 1])
-    tmp_array.append(maxi)
+# for i in range(len(array)):
+#     tmp_array = []
+#     maxi = sum(array[:i + 1])
+#     tmp_array.append(maxi)
 
-    end = i + 1
-    cnt = 1
-    for j in range(i + 1, len(array)):
-        if sum(array[end:j + 1]) < maxi:
-            tmp_array.append(sum(array[end:j + 1]))
-            end = j - 1
+#     end = i + 1
+#     cnt = 1
+#     for j in range(i + 1, len(array)):
+#         if sum(array[end:j + 1]) < maxi:
+#             tmp_array.append(sum(array[end:j + 1]))
+#             end = j - 1
+
+#     if len(tmp_array) == m:
+#         min_result = min(min_result, max(tmp_array))
+
+# print(min_result)
+
+for maxi in range(0, 101): # max
+    end = 0
+    tmp_array = []
+    for i in range(len(array)):
+        if sum(array[end:i + 1]) > maxi:
+            tmp_array.append(sum(array[end:i + 1]))
+            end = i + 1
+        elif i + 1 == n:
+            tmp_array.append(sum(array[end:i + 1]))
 
     if len(tmp_array) == m:
         min_result = min(min_result, max(tmp_array))
