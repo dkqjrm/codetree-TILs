d23b9a8c0a55c3d8416bb474ed085f7a61b47cb2
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-03 14:29:41 +0900

    Title: 이상한 폭탄 3, Time: 4049ms, Memory: 143MB, Status: Wrong Answer - CodeTree

--- 240403/이상한 폭탄 3/strange-bomb-3.py
+++ 240403/이상한 폭탄 3/strange-bomb-3.py
@@ -1,18 +1,18 @@
 n, k = map(int, input().split())
 array = [int(input()) for _ in range(n)]
 
-bomb = {}
+bomb_dict = {}
 
 for i in range(1, max(array) + 1): # 조사할 정수
     now = -1e12
+    bomb = [0] * 1000
     for idx, num in enumerate(array):
         if num == i:
             distance = idx - now
             if distance <= k:
-                if i not in bomb.keys():
-                    bomb[i] = 1
-                else:
-                    bomb[i] += 1
+                bomb[now] = 1
+                bomb[idx] = 1
             now = idx
+    bomb_dict[i] = sum(bomb)
 
-print(sorted(bomb.items(), key = lambda x : (-x[1], -x[0]))[0][1])
+print(sorted(bomb_dict.items(), key = lambda x : (-x[1], -x[0]))[0][0])
