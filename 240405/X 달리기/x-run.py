ee436bab528df4729c462e6153c97d3b5d404557
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-05 00:10:09 +0900

    Title: X 달리기, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240405/X 달리기/x-run.py
+++ 240405/X 달리기/x-run.py
@@ -3,15 +3,19 @@ x = int(input())
 V = 1
 location = 1
 cnt = 0
+check = False
 
 while location <= x:
     # print(location, V)
-    if location + int(((V+1)*(V/2))) >= x and V - 1 >= 1:
-        V -= 1
-    elif location >= 2 and V == 1:
-        pass
+    if check:
+        if V != 1:
+            V -= 1
     else:
-        V += 1
+        if location + int(((V+2)*((V+1)/2))) > x and V - 1 >= 1:
+            check = True
+            V -= 1
+        else:
+            V += 1
     location += V
     cnt += 1
 
@@ -20,3 +24,4 @@ print(cnt)
 # 1 2 3 = 6 (4*1.5)
 # 1 2 3 4 = 10 (5*2)
 # 1 2 3 4 5 = 15 (6*2.5)
+# 1 2 3 4 3 2 1 = 16
