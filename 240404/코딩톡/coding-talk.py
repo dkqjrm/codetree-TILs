f09d0c8634d2b342446149a2a253ee53ab0d1dea
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-04 23:48:42 +0900

    Title: 코딩톡, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240404/코딩톡/coding-talk.py
+++ 240404/코딩톡/coding-talk.py
@@ -2,6 +2,8 @@ n, m, p = map(int, input().split())
 array = [input().split() for _ in range(m)]
 
 # p와 같거나 뒤에 말한 사람들은 다 읽었음.
+
+
 people = []
 for i in range(n):
     people.append(chr(65+i))
@@ -9,5 +11,15 @@ for i in range(n):
 for idx, (c, u) in enumerate(array):
     if idx >= (p-1) and c in people:
         people.remove(c)
-
+    if idx == (p-1) and u == 0:
+        people = []
 print(*people)
+
+
+# 6 6 1
+# D 0
+# C 1
+# B 2
+# B 2
+# A 2
+# F 4
