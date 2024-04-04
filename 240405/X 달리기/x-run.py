29dae885d3490e7db321b54b3412c72daec95d83
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-05 00:28:18 +0900

    Title: X 달리기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240405/X 달리기/x-run.py
+++ 240405/X 달리기/x-run.py
@@ -1,23 +1,22 @@
 x = int(input())
 
 V = 1
-location = 1
+location = 0
 cnt = 0
 check = False
 
-while location <= x:
-    # print(location, V)
-    if check:
-        if V != 1:
-            V -= 1
-    else:
-        if location + int(((V+2)*((V+1)/2))) > x and V - 1 >= 1:
-            check = True
-            V -= 1
-        else:
-            V += 1
+while location != x:
     location += V
+    
+    # print(f'도착 위치 {location}, 현재 속도 {V}')
+    if x - location < int((V+1+1)*((V+1)/2)) and V > 1:
+        V -= 1
+    elif x - location >= int((V+1+1)*((V+1)/2)):
+        V += 1
+
     cnt += 1
+
+# 내가 만약 2에서 5로 가야함, 5-2가 V+1~ [3, 2, 1] 보다 크다면 속도 유지 = int((V+1+1)*((V+1)/2))
 
 print(cnt)
 
@@ -25,3 +24,5 @@ print(cnt)
 # 1 2 3 4 = 10 (5*2)
 # 1 2 3 4 5 = 15 (6*2.5)
 # 1 2 3 4 3 2 1 = 16
+# 1 2 3 2 1 = 9
+# 1 2 1 2 1 1 = 8
