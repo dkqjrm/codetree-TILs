8b8752b8392a9252da43e8e78a9cec2cf07494ea
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-05 00:39:40 +0900

    Title: X 달리기, Time: 105ms, Memory: 24MB, Status: Passed - CodeTree

--- 240405/X 달리기/x-run.py
+++ 240405/X 달리기/x-run.py
@@ -9,14 +9,20 @@ while location != x:
     location += V
     
     # print(f'도착 위치 {location}, 현재 속도 {V}')
-    if x - location < int((V+1+1)*((V+1)/2)) and V > 1:
-        V -= 1
-    elif x - location >= int((V+1+1)*((V+1)/2)):
+    # 한칸 올린게 괜찮으면 올리기
+    if x - location >= int((V+1+1)*((V+1)/2)):
         V += 1
+    # 그대로 괜찮으면 유지하기
+    elif x - location >= int((V+1)*((V)/2)):
+        V = V
+    # 아니면 내리기
+    else:
+        V -= 1
+
 
     cnt += 1
 
-# 내가 만약 2에서 5로 가야함, 5-2가 V+1~ [3, 2, 1] 보다 크다면 속도 유지 = int((V+1+1)*((V+1)/2))
+# 내가 만약 2에서 5로 가야함, 5-2가 V+1~ [3, 2, 1] 보다 크다면 속도 유지  x - location < int((V+1+1)*((V+1)/2))
 
 print(cnt)
 
@@ -26,3 +32,8 @@ print(cnt)
 # 1 2 3 4 3 2 1 = 16
 # 1 2 3 2 1 = 9
 # 1 2 1 2 1 1 = 8
+# 1 2 2 2 1  = 8
+
+# 1 2 2 1 -> 6
+
+# 유지하는게 있어야하네,,
