ca5215f60a80d11e902e913e47461cb0ee71815b
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-07 16:36:13 +0900

    Title: 기수 정렬 구현, Time: 243ms, Memory: 38MB, Status: Passed - CodeTree

--- 240407/기수 정렬 구현/implement-radix-sort.py
+++ 240407/기수 정렬 구현/implement-radix-sort.py
@@ -1,19 +1,16 @@
-MAX_K = 6
-MAX_DIGIT = 10
-
 n = int(input())
 array = list(map(int, input().split()))
 
 def custom_sort(array):
     p = 1
-    for pos in range(MAX_K):
-        arr_new = [[] for _ in range(MAX_DIGIT)]
-        for i in array:
-            digit = (i // p) % 10
+    for _ in range(6): # 최대 6자리 숫자까지
+        arr_new = [[] for _ in range(10)] # 숫자가 들어갈 리스트 만들기
+        for i in array: # 각 숫자를 판단하는거
+            digit = (i // p) % 10 # 판단해야하는 자릿수의 수, 만약 없으면 0으로 들어감
             arr_new[digit].append(i)
 
         array = []
-        for digit in range(MAX_DIGIT):
+        for digit in range(10):
             for i in arr_new[digit]:
                 array.append(i)
 
