d613637c506075497b95ef99143d1ccf6d2e404d
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-07 13:47:31 +0900

    Title: 선택 정렬 구현, Time: 126ms, Memory: 25MB, Status: Passed - CodeTree

--- 240407/선택 정렬 구현/implement-selection-sort.py
+++ 240407/선택 정렬 구현/implement-selection-sort.py
@@ -3,10 +3,10 @@ array = list(map(int, input().split()))
 
 
 
-for i in range(n):
-    min_index = -1
-    for j in range(i, n):
-        if min_index == -1 or array[min_index] >= array[j]:
+for i in range(n - 1):
+    min_index = i
+    for j in range(i + 1, n):
+        if array[min_index] > array[j]:
             min_index = j
     array[i], array[min_index] = array[min_index], array[i]
 
