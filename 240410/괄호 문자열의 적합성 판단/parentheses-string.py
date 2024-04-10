97186b786f0d9e3aad5693d88b0944963b98b56a
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-10 10:15:46 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240410/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -0,0 +1,23 @@
+question = input()
+
+
+
+def solve(question):
+    arr = []
+    for i in question:
+        if i == '(':
+            arr.append(i)
+
+        elif i == ')':
+            if len(arr) > 0:
+                arr.pop()
+            else:
+                return "No"
+        
+        if len(arr) == 0:
+            return "Yes"
+        else:
+            return "No"
+
+
+print(solve(question))
