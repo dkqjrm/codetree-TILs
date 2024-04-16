d3a84a88d6c5cfc60fbefefc8c737624eb119059
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-16 14:48:43 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240416/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+
+q = deque()
+n, m = map(int, input().split())
+
+matrix = [list(map(int, input().split())) for _ in range(n)]
+visited = [[0] * n for _ in range(n)]
+
+q.append([0, 0])
+visited[0][0] = 1
+
+def in_range(r, c):
+    return 0 <= r < n and 0 <= c < n
+
+drs = [1, -1, 0, 0]
+dcs = [0, 0, -1, 1]
+
+while q:
+    curr_v = q.popleft()
+    for dr, dc in zip(drs, dcs):
+        next_r = curr_v[0] + dr
+        next_c = curr_v[1] + dc
+        if in_range(next_r, next_c) and matrix[next_r][next_c] == 1 and visited[next_r][next_c] == 0:
+            q.append([next_r, next_c])
+            visited[next_r][next_c] = 1
+
+
+print(int(visited[n-1][n-1] == 1))
