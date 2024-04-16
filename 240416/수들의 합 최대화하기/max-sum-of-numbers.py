3dacab31cbaaf4c06f6ab4465b7b6a1f2c51b546
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-16 17:52:23 +0900

    Title: 수들의 합 최대화하기, Time: 119ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240416/수들의 합 최대화하기/max-sum-of-numbers.py
@@ -0,0 +1,29 @@
+n = int(input())
+matrix = [list(map(int, input().split())) for _ in range(n)]
+visited = [0] * (n + 1)
+# 1 ~ n 까지의 순열을 만들고, 각 row에 순열 값을 대입해서 max 값 찾으면 될 듯.
+
+total = 0
+answer = []
+def find(num):
+    global total
+    if num == n + 1:
+        tmp_total = 0
+        for idx, i in enumerate(answer):
+            tmp_total += matrix[idx][i-1]
+
+        total = max(tmp_total, total)
+        return
+    
+    for i in range(n + 1):
+        if visited[i] == 0:
+            visited[i] = 1
+            answer.append(i)
+            
+            find(num + 1)
+
+            answer.pop()
+            visited[i] = 0
+
+find(1)
+print(total)
