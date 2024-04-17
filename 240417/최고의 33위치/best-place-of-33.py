a8c768cd6fdd27997a0a0e5017ac41dc1f4d20d5
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-04-17 21:34:01 +0900

    Title: 최고의 33위치, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

--- 240417/최고의 33위치/best-place-of-33.py
+++ 240417/최고의 33위치/best-place-of-33.py
@@ -3,7 +3,7 @@ n = int(input())
 matrix = [list(map(int, input().split())) for _ in range(n)]
 
 def in_range(x, y):
-    return 0 <= x < 3 and 0 <= y < 3
+    return 0 <= x < n and 0 <= y < n
 
 drs = [-1, 1, 0, 0, -1, -1, 1, 1]
 dcs = [0, 0, -1, 1, -1, 1, -1, 1]
@@ -18,7 +18,6 @@ for i in range(n):
             nc = j + dc
             if in_range(nr, nc):
                 total += matrix[nr][nc]
-        # print(total)
         max_total = max(total, max_total)
     
 print(max_total)
