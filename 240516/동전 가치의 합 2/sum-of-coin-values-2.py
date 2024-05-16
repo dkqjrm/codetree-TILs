d7d3a6fec8f8378d3dc729a273e9dba253b6d692
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-05-16 13:42:40 +0900

    Title: 동전 가치의 합 2, Time: 5037ms, Memory: 32MB, Status: Time Limit Exceed - CodeTree

---
+++ 240516/동전 가치의 합 2/sum-of-coin-values-2.py
@@ -0,0 +1,17 @@
+m = int(input())
+n = int(input())
+
+dp = [0] * (m + 1)
+dp[0] = 1
+
+coins = [list(map(int, input().split())) for _ in range(n)]
+
+for p, k in coins: # 동전 순서대로
+    for current_money in range(m, -1, -1):
+        for count in range(1, k + 1):
+            if current_money + p * count <= m:
+                dp[current_money + p * count] += dp[current_money]
+            else:
+                break
+
+print(dp[m])
