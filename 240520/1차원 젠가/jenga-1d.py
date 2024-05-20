40c6531d32a5984c3ce01e71800e445302a633ca
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-05-20 17:58:57 +0900

    Title: 1차원 젠가, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240520/1차원 젠가/jenga-1d.py
+++ 240520/1차원 젠가/jenga-1d.py
@@ -2,17 +2,24 @@ n = int(input()) # 처음 놓여있는 블럭의 수
 array = [int(input()) for _ in range(n)]
 s1, e1 = map(int, input().split()) # s1부터 e1까지의 블럭을 제외
 s2, e2 = map(int, input().split()) # s2부터 e2까지의 블럭을 제외
+tmp_array = []
 
-if s1 != 1:
-    array = array[:-e1] + array[-s1+1:]
-else:
-    array = array[:-e1]
+for i in range(s1 - 1, e1):
+    array[i] = None
 
-if s2 != 1:
-    array = array[:-e2] + array[-s2+1:]
-else:
-    array = array[:-e2]
-
-print(len(array))
 for i in array:
+    if i != None:
+        tmp_array.append(i) 
+array = tmp_array
+
+tmp_array = []
+for i in range(s2 - 1, e2):
+    array[i] = None
+
+for i in array:
+    if i != None:
+        tmp_array.append(i) 
+
+print(len(tmp_array))
+for i in tmp_array:
     print(i)
