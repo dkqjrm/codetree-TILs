5c5d1ba573e6f6ba1fa80f2f4b638b1b9ddebfb1
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-05-26 20:50:04 +0900

    Title: 학생들의 점수대 파악하기, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240526/학생들의 점수대 파악하기/understand-students-scores.py
+++ 240526/학생들의 점수대 파악하기/understand-students-scores.py
@@ -1,7 +1,7 @@
 n = int(input())
 array = list(map(int, input().split()))
 
-for i in range(100, -1, -10):
+for i in range(100, 0, -10):
     number = sum([i + 10 > num >= i for num in array])
     if number != 0:
         print("{} - {}".format(i, number))
