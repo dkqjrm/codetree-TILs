1fa3373fc80863644d585b54a090720e26816f8f
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-05-27 19:21:13 +0900

    Title: 계단 오르기, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

--- 240527/계단 오르기/climbing-stairs.py
+++ 240527/계단 오르기/climbing-stairs.py
@@ -1,28 +1,12 @@
-# dp = [0] * 1005
-# dp[0] = 0
-# dp[1] = 0
-# dp[2] = 1
-# dp[3] = 1
-
-# n = int(input())
-
-# for i in range(4, n + 1):
-#     dp[i] = dp[i-2] + dp[i-3]
-
-# print(dp[n] % 10007)
+dp = [0] * 1005
+dp[0] = 1
+dp[1] = 0
+dp[2] = 1
+dp[3] = 1
 
 n = int(input())
-memo = [0] * 1005
-memo[0] = 0
-memo[1] = 0
-memo[2] = 1
-memo[3] = 1
 
-def solve(n):
-    if memo[n] != 0:
-        return memo[n]
-    
-    memo[n] = memo[n-2] + memo[n-3]
-    return memo[n]
+for i in range(4, n + 1):
+    dp[i] = dp[i-2] + dp[i-3]
 
-print(solve(n))
+print(dp[n] % 10007)
