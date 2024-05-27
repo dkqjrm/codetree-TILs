08f164cc29c0f7685d1ddc3b8d8a179d5c706929
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-05-27 16:47:23 +0900

    Title: 피보나치 수, Time: 5087ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

--- 240527/피보나치 수/fibonacci-number.py
+++ 240527/피보나치 수/fibonacci-number.py
@@ -1,11 +1,21 @@
 n = int(input())
 
-dp = [0] * 50
+# dp = [0] * 50
 
-dp[0] = 0
-dp[1] = 1
+# dp[0] = 0
+# dp[1] = 1
 
-for i in range(2, n + 1):
-    dp[i] = dp[i-1] + dp[i-2]
+# for i in range(2, n + 1):
+#     dp[i] = dp[i-1] + dp[i-2]
 
-print(dp[n])
+# print(dp[n])
+
+def solve(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    
+    return solve(n-1) + solve(n-2)
+
+print(solve(n))
