104e81511f259b7a7a04d1f3f62aa4a5e9e48eda
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-05-27 15:55:58 +0900

    Title: xor 결과 최대 만들기, Time: 5064ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 240527/xor 결과 최대 만들기/max-of-xor.py
+++ 240527/xor 결과 최대 만들기/max-of-xor.py
@@ -5,25 +5,24 @@ array = list(map(int, input().split()))
 ans = []
 max_num = 0
 
-def xor(ans):
-    base = int(bin(ans[0])[2:])
-    for idx in range(1, len(ans)):
-        base = base ^ int(bin(ans[idx])[2:])
-    return int(str(base), 2)
-        
+# def xor(ans):
+#     base = ans[0]
+#     for idx in range(1, len(ans)):
+#         base = base ^ ans[idx]
+#     return base
 
 
-def solve(cnt):
+def solve(cnt, xor_num):
     global max_num
     if cnt == m:
-        max_num = max(max_num, xor(ans))
+        max_num = max(max_num, xor_num)
         return
 
     for i in array:
         ans.append(i)
-        solve(cnt + 1)
+        solve(cnt + 1, xor_num ^ i)
         ans.pop()
     
 
-solve(0)
+solve(0, 0)
 print(max_num)
