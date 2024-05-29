eaa2913f43a4711ffdbeb385627bb739b4e2b361
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-05-29 15:50:46 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 322ms, Memory: 29MB, Status: Wrong Answer - CodeTree

--- 240529/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240529/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,6 +1,7 @@
 n = int(input())
 matrix = [list(map(int, input().split())) for _ in range(n)]
-dp = [[1] * n for _ in range(n)]
+dp = [[-1] * n for _ in range(n)]
+
 
 drs = [-1, 1, 0, 0]
 dcs = [0, 0, -1, 1]
@@ -9,19 +10,31 @@ def in_range(r, c):
     return 0 <= r < n and 0 <= c < n
 
 def dfs(r, c):
-    global dp
-    now = matrix[r][c] # starting point의 value
+    # global dp
+    # now = matrix[r][c] # starting point의 value
+    # for dr, dc in zip(drs, dcs):
+    #     next_r = r + dr
+    #     next_c = c + dc
+    #     if in_range(next_r, next_c) and matrix[next_r][next_c] > now:
+    #         dp[next_r][next_c] = max(dp[next_r][next_c], dp[r][c] + 1)
+    #         dfs(next_r, next_c)
+    # return
+    if dp[r][c] != -1:
+        return dp[r][c]
+
+    max_move = 1
     for dr, dc in zip(drs, dcs):
         next_r = r + dr
         next_c = c + dc
-        if in_range(next_r, next_c) and matrix[next_r][next_c] > now:
-            dp[next_r][next_c] = max(dp[next_r][next_c], dp[r][c] + 1)
-            dfs(next_r, next_c)
-    return
+        if in_range(next_r, next_c) and matrix[next_r][next_c] > matrix[r][c]:
+            max_move = max(max_move, 1 + dfs(next_r, next_c))
+
+    dp[r][c] = max_move
+    return dp[r][c]
 
 for r in range(n):
     for c in range(n):
-        if dp[r][c] == 1:
+        if dp[r][c] == -1:
             dfs(r, c)
 
 # for i in dp:
