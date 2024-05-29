261046bb6c605f9ec33e9f6f47868023fa879eb2
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-05-29 15:58:19 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 353ms, Memory: 34MB, Status: Passed - CodeTree

--- 240529/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240529/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,6 +1,6 @@
 n = int(input())
 matrix = [list(map(int, input().split())) for _ in range(n)]
-dp = [[-1] * n for _ in range(n)]
+dp = [[0] * n for _ in range(n)]
 
 
 drs = [-1, 1, 0, 0]
@@ -10,16 +10,7 @@ def in_range(r, c):
     return 0 <= r < n and 0 <= c < n
 
 def dfs(r, c):
-    # global dp
-    # now = matrix[r][c] # starting point의 value
-    # for dr, dc in zip(drs, dcs):
-    #     next_r = r + dr
-    #     next_c = c + dc
-    #     if in_range(next_r, next_c) and matrix[next_r][next_c] > now:
-    #         dp[next_r][next_c] = max(dp[next_r][next_c], dp[r][c] + 1)
-    #         dfs(next_r, next_c)
-    # return
-    if dp[r][c] != -1:
+    if dp[r][c]:
         return dp[r][c]
 
     max_move = 1
@@ -35,8 +26,6 @@ def dfs(r, c):
 max_result = 0
 for r in range(n):
     for c in range(n):
-        max_result = max(max_result, dfs(r, c))
+        max_result = max(max_result, dfs(r, c))  # 모든 셀에 대해 최대 이동 거리 계산
 
-# for i in dp:
-#     print(*i)
-print(max(max(dp)))
+print(max_result)
