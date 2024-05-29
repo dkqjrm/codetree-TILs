3ea52d71fe00bbc25c632a214df15c5d48327c01
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-05-29 14:54:45 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 5085ms, Memory: 35MB, Status: Time Limit Exceed - CodeTree

---
+++ 240529/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -0,0 +1,30 @@
+n = int(input())
+matrix = [list(map(int, input().split())) for _ in range(n)]
+dp = [[1] * n for _ in range(n)]
+
+drs = [-1, 1, 0, 0]
+dcs = [0, 0, -1, 1]
+
+def in_range(r, c):
+    return 0 <= r < n and 0 <= c < n
+
+def dfs(r, c):
+    global dp
+
+    now = matrix[r][c] # starting point의 value
+    for dr, dc in zip(drs, dcs):
+        next_r = r + dr
+        next_c = c + dc
+        if in_range(next_r, next_c) and matrix[next_r][next_c] > now:
+            dp[next_r][next_c] = max(dp[next_r][next_c], dp[r][c] + 1)
+            dfs(next_r, next_c)
+    return
+
+for r in range(n):
+    for c in range(n):
+        # r, c는 스타팅 포인트
+        dfs(r, c)
+
+# for i in dp:
+#     print(*i)
+print(max(max(dp)))
