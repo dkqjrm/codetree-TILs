b0d13e7dd10a2dd78f4b652f348745c55d0abd8d
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-05-30 18:41:17 +0900

    Title: 2차원 최대 증가 수열, Time: 141ms, Memory: 73MB, Status: Passed - CodeTree

--- 240530/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
+++ 240530/2차원 최대 증가 수열/longest-increasing-sequence-2d.py
@@ -1,18 +1,20 @@
 n, m = map(int, input().split())
 array = [list(map(int, input().split())) for _ in range(n)]
-dp = [[-1] * n for _ in range(n)]
+dp = [[-1] * m for _ in range(n)]
 dp[0][0] = 1
 
 for i in range(n - 1):
-    for j in range(n - 1):
+    for j in range(m - 1):
         num = array[i][j]
         cnt = dp[i][j]
         if dp[i][j] == -1:
             continue
-            
+
         for k in range(i + 1, n):
-            for l in range(j + 1, n):
+            for l in range(j + 1, m):
                 if array[k][l] > num:
                     dp[k][l] = max(dp[k][l], cnt + 1)
 
+# for i in dp:
+#     print(*i)
 print(max([max(i) for i in dp]))
