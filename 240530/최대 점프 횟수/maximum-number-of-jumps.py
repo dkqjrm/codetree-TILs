e14e14128834e67f91175148b686fe98890521b9
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-05-30 17:01:17 +0900

    Title: 최대 점프 횟수, Time: 156ms, Memory: 25MB, Status: Passed - CodeTree

--- 240530/최대 점프 횟수/maximum-number-of-jumps.py
+++ 240530/최대 점프 횟수/maximum-number-of-jumps.py
@@ -1,22 +1,16 @@
 n = int(input())
 array = list(map(int, input().split()))
-dp = [0] * n
+dp = [-1] * n
+dp[0] = 0
 
 def in_range(idx):
     return 0 < idx < n
 
-for i in range(n):
-    for j in range(1, array[i] + 1):
-        idx = i + j
-        if in_range(idx):
-            dp[idx] = max(dp[i] + 1, dp[idx])
-
-# print(dp)
-num = 0
 for i in range(1, n):
-    if dp[i] != 0:
-        num = max(num, dp[i])
-    else:
-        break
+    for j in range(i):
+        if dp[j] != -1 and j + array[j] >= i:
+            dp[i] = max(dp[i], dp[j] + 1)
+        
 
-print(num)
+print(max(dp))
+# print(dp)
