821050c27b338f12c55342964a8d2d9b3f501084
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-05-31 17:04:59 +0900

    Title: 동전 거슬러주기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240531/동전 거슬러주기/coin-change.py
@@ -0,0 +1,20 @@
+# 지금까지 선택한 동전의 합이 같다면, 지금까지 사용한 동전의 개수가 많을수록 더 좋다.
+
+n, m = map(int, input().split())
+coins = list(map(int, input().split()))
+dp = [1e12] * (m + 1)
+dp[0] = 1
+for coin in coins:
+    dp[coin] = 1
+
+
+for i in range(max(coins), m + 1):
+    for coin in coins:
+        dp[i] = min(dp[i], dp[i-coin] + 1)
+
+coin = dp[m]
+
+if coin == 1e12:
+    print(-1)
+else:
+    print(coin)
