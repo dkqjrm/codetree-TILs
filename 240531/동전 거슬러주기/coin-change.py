1705999447f6a9d6b1e4ce7bdeef51d88978cb05
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-05-31 17:05:51 +0900

    Title: 동전 거슬러주기, Time: 97ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240531/동전 거슬러주기/coin-change.py
+++ 240531/동전 거슬러주기/coin-change.py
@@ -5,8 +5,8 @@ coins = list(map(int, input().split()))
 dp = [1e12] * (m + 1)
 dp[0] = 1
 for coin in coins:
-    dp[coin] = 1
-
+    if coin < m:
+        dp[coin] = 1
 
 for i in range(max(coins), m + 1):
     for coin in coins:
