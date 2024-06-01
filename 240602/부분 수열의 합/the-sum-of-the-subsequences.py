c21f5446519f821578f7e0596c34f6e577e4e0e4
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-06-02 03:32:56 +0900

    Title: 부분 수열의 합, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240602/부분 수열의 합/the-sum-of-the-subsequences.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+array = list(map(int, input().split()))
+dp = [10005] * (m + 1)
+dp[0] = 0
+
+for num in array:
+    for i in range(m, -1, -1):
+        dp[i] = min(dp[i - num] + 1, dp[i])
+
+print(dp)
+
+if dp[m] != 10005:
+    print("Yes")
+else:
+    print("No")
+
+# 5 10
+# 5 7 10 12
+# 5 7 9 10 11 12
