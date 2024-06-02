3f86df860335adf15416f87094128ad405437fff
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-06-02 15:49:54 +0900

    Title: 배낭 채우기, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240602/배낭 채우기/knapsack.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+array = [list(map(int, input().split())) for _ in range(n)]
+dp = [-1] * (m + 1)
+dp[0] = 0
+# dp[i] 무게 i일 때 얻을 수 있는 최대의 가치
+
+for w, v in array:
+    for i in range(m, -1, -1):
+        if i - w >= 0 and dp[i - w] != -1:
+            dp[i] = max(dp[i - w] + v, dp[i])
+
+print(dp[m])
