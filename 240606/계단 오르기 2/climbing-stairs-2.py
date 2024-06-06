55563dc85f18c3bb41871bff35bf527cc68934f4
Hyun-Sik Won <53959746+dkqjrm@users.noreply.github.com>
2024-06-06 17:51:12 +0900

    Title: 계단 오르기 2, Time: 122ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240606/계단 오르기 2/climbing-stairs-2.py
+++ 240606/계단 오르기 2/climbing-stairs-2.py
@@ -3,16 +3,15 @@ array = list(map(int, input().split()))
 # 한 번에 정확히 1계단 혹은 2계단 단위로 올라갈 수 있음.
 # 그런데 계단을 1개씩 올라가는건 최대 3번만 하고 싶음.
 
-dp = [[-1e12] * 3 for _ in range(n)]
+dp = [[-10000000] * 3 for _ in range(n)]
 
 # dp[i] = max(dp[i - 1], dp[i - 2]) + array[i]
 # 인데, 여기에 j를 추가해서 1번씩 몇번이나 뛰었는지 확인을 해야 할 것 같음.
 # dp[i][j] = max(dp[i - 1][j - 1], dp[i - 2][j])
 
 
-dp[0][0] = 0
-dp[1][1] = array[0]
-dp[2][0] = array[1]
+dp[0][1] = array[0]
+dp[1][0] = array[1]
 
 for i in range(2, n):
     for j in range(3):
@@ -20,4 +19,7 @@ for i in range(2, n):
             dp[i][j] = dp[i - 1][j - 1]
         dp[i][j] = max(dp[i][j],  dp[i - 2][j]) + array[i]
 
+
+# for i in dp:
+#     print(*i)
 print(max(dp[-1]))
